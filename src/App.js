9a4af34fb88105ca99e6303a696b5438cc1935d3
poowadon.git <poowadon.ingeni@gmail.com>
2022-06-06 15:47:46 +0700

    sdf

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React CI/CD sfadsfdsaf
+          Learn React CI/CD test
         </a>
       </header>
     </div>
