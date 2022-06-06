591e22a639d9cc90c4d2b49271133d3a920b5c23
poowadon.git <poowadon.ingeni@gmail.com>
2022-06-06 15:43:26 +0700

    feat: change text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React CI/CD
+          Learn React CI/CD sfadsfdsaf
         </a>
       </header>
     </div>
